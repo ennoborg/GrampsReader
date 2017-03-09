45b327d07e685a0bf2c3cd995b7b1980b09f3671
Enno Borgsteede <ennoborg@gmail.com>
2017-03-09 20:23:46 +0100

    1e versie

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Xml.Linq;
+
+namespace GrampsReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = @"..\..\..\data.gramps";
+
+            if (!File.Exists(filePath))
+            {
+                Debug.WriteLine("File not found: " + filePath);
+                return;
+            }
+
+            using (Stream fileStream = File.OpenRead(filePath),
+              zippedStream = new GZipStream(fileStream, CompressionMode.Decompress))
+            {
+                using (StreamReader reader = new StreamReader(zippedStream))
+                {
+                    XElement grampsTree = XElement.Load(reader);
+
+                    XNamespace gr = "http://gramps-project.org/xml/1.5.1/";
+
+                    // Zoek alle nodes van het type phone.
+                    var test = grampsTree.Descendants(gr + "phone");
+
+                    foreach (var num in test)
+                    {
+                        Debug.WriteLine("Telefoon: " + num.Value);
+                    }
+                }
+            }
+        }
+    }
+}
